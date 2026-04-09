98806a9927f2a18f85e9ec8d20d9fffac933b80d
jacqueline-bruce <jacquelinebruce2012@gmail.com>
2026-04-09 17:58:29 -0600

    added navbar and some artwork

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      nunito: ["Nunito", "sans-serif"],
+      playright: ["Playwright IE", "cursive"],
+    },
+  },
+  plugins: [],
+}
